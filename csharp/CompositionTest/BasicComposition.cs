b8165bf292bda3d7453f83380e37a52b4279bef0
roschart <jlbalirac@hotmail.com>
2017-09-08 23:44:44 +0200

    Refactor: Change the test to view transformation to different types

--- csharp/CompositionTest/BasicComposition.cs
+++ csharp/CompositionTest/BasicComposition.cs
@@ -12,7 +12,7 @@ namespace CompositionTest
         {
             public static int Inc(int x) => x + 1;
             public static int Double(int x) => x * 2;
-            public static int Pow(int x) => x * x;
+            public static double Pow(int x) => x * x;
         }
 
         public class BasicComposition
@@ -20,27 +20,28 @@ namespace CompositionTest
             [Fact]
             public void PointFreeComposition()
             {
-                var logic = J.Composite<int, int, int, int>(Business.Inc
-                                                        , Business.Double
-                                                        , Business.Pow);
-                Assert.Equal(expected: 19, actual: logic(3));
+                var logic = J.Composite<int, int, int, double>
+                                            (Business.Pow
+                                            , Business.Double
+                                            , Business.Inc);
+                Assert.Equal(expected: 64, actual: logic(3));
             }
 
             [Fact]
             public void IdentityComposition(){
                 var result= Identity<int>.Of(3)
-                                         .Map(Business.Pow)
+                                         .Map(Business.Inc)
                                          .Map(Business.Double)
-                                         .Map(Business.Inc);
-                Assert.Equal(expected:19,actual:result.Value);
+                                         .Map(Business.Pow);
+                Assert.Equal(expected:64,actual:result.Value);
             }
             [Fact]
             public void IdentityCompositionIsAStupidThing(){
                 var result = new List<int>(){3}
-                                         .Select(Business.Pow)
+                                         .Select(Business.Inc)
                                          .Select(Business.Double)
-                                         .Select(Business.Inc);
-                Assert.Equal(expected:19,actual:result.First());
+                                         .Select(Business.Pow);
+                Assert.Equal(expected:64,actual:result.First());
             }
         }
     }
