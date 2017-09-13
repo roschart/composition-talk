68a0cf827d25a77a16059f84ebf8f7195802d525
roschart <jlbalirac@hotmail.com>
2017-09-13 23:25:20 +0200

    Refactor: Move class Business to other project

--- csharp/CompositionTest/BasicCompositionTest.cs
+++ csharp/CompositionTest/BasicCompositionTest.cs
@@ -8,14 +8,7 @@ using Xunit;
 
 namespace CompositionTest
 {
-        public class Business
-        {
-            public static int Inc(int x) => x + 1;
-            public static int Double(int x) => x * 2;
-            public static double Pow(int x) => x * x;
-        }
-
-        public class BasicComposition
+        public class BasicCompositionTest
         {
             [Fact]
             public void PointFreeComposition()
